dc50e33f3e56a219b97be9a278c1c8cac302a4e0
Abhishek Nemane <abhisheknemane0@gmail.com>
2026-07-17 22:15:08 +0530

    new file to change data
    Study of DOM (children,parent, child)

---
+++ pr.js
@@ -0,0 +1,141 @@
+// variable and data types
+// console.log("welcome to javascript");
+// let a =10;
+// let b =30;
+// let c = "harry";
+// console.log("sum of two number:"+(a+b));
+// console.log(typeof a,typeof b,typeof c);
+// {
+//     let a =20;
+//     console.log("Print the number"+ a);
+// }
+// console.log("Print the number"+ a);
+// let a =34;
+// let b =3.24;
+// let c =null;
+// let d ="String";
+// let e =undefined;
+// let f = true;
+// let j = 5486;
+
+//console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f,typeof j)
+
+// conditinal statement
+// let age=22;
+// if (age>18) {
+//     console.log("you can drive the car");
+// }
+// else if (age>18 && age<20) {
+//     console.log("you are not adult");
+// }
+// else {
+//  console.log(" age is invalid");
+// }
+// loops and funcation
+
+// let i=5;
+// for(let i=0 ;i<6 ;i++){
+//     console.log("Print no 1 to 5:"+i);
+// }
+// let obj = {
+//      name:"abhi",
+//      age: 14,
+//      city:"new York",
+//     array:[2,3,4,5,6]
+// };
+// console.log(obj);
+// for (const key in obj) {
+//     if (!Object.hasOwn(obj, key)) continue;
+    
+//     const element = obj[key];
+    
+    
+// }
+// let c=[2,3,4,5,6]
+// for (const element of c) {
+//     console.log(c);
+// }
+// let age = 19;
+// while(age>18) {
+//     console.log("you can drive car");
+//     return
+// }
+// let age = 13;
+// do {
+//     console.log("welcome to website");
+// }while(age>18)
+
+// function Myfuncn() {
+//     console.log("welcome to javascript");
+
+// }
+// function sum(a,b) {
+//     console.log("enter the first number");
+//     console.log("enter the second number");
+//     console.log("sum of a & b:"+ (a+b) );
+// }
+// Myfuncn();
+// sum( );
+// obj = {
+//    "harry": 98,
+//    "rohan": 70,
+//    "aakash":7
+// }
+// function marks(name,marks) {
+//     console.log(name+"mark"+marks);
+// }
+// marks(harry,98);
+// marks(rohan,70);
+// marks(aakash,7);
+
+// let arr = [2,3,4,5,6]
+
+// console.log(arr.length);
+// console.log(arr[1]);
+// console.log(arr.toString);
+// console.log(arr);
+// console.log(arr[0]=987);
+// console.log(arr.join( " and " ));
+// arr[0]=342;
+// console.log(arr[0])
+//    console.log(arr);
+//    console.log(arr.pop());
+//    console.log(arr.length);
+
+//    console.log(arr); 
+// //    console.log(arr.push());
+
+// // console.log(arr.shift());
+// console.log(arr.unshift(23));
+//  console.log(arr);
+// console.log(arr.push(9));
+//  console.log(arr); 
+// //  console.log(arr.delete(0));
+//  let arr1 =[7,8,9,6,7]
+//  console.log(delete arr1[4]);
+//  console.log(arr1.push(4));
+//  console.log(arr1);
+// console.log(arr.concat (arr,arr1))
+
+// let arr = [1,2,3,4,5]
+// let newarr = arr.map((e,index,value)=>{
+//     return e ** 2
+// })
+// console.log(newarr);
+// const greatertheySeven=(e)=> {
+//     if(e>3){
+//         return true
+//     }
+//     return false
+// } 
+// console.log(arr.filter(greatertheySeven))
+// let arr2 ="Abhishek";
+// console.log(arr2.from("Abhishek"));
+
+// let arr =[1,2,3,4,5]
+// const red = (a,b)=> {
+//     return a+b
+// }
+// console.log(arr.reduce(red));
+console.log("hello js")
+
